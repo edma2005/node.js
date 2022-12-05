40f15f26798ad54da009f509267572d46f7fe508
Edma <edma20052005@yahoo.co.uk>
2022-12-05 21:43:45 +0200

    changes

--- 1_paskaita/index.js
+++ 1_paskaita/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const casual = require("casual");
+const city = casual.city;
+casual.define('point', function() {
+    return {
+        x: Math.random() * 10 + 1,
+        y: Math.random() * 10 + 1
+    };
+});
+const point = casual.point;
+
+console.log(city);
+console.log(point);
