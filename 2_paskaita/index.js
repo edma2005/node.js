bd91dc3d2ebe9f067b9f4d2f53b8cd1522512e19
Edma <edma20052005@yahoo.co.uk>
2022-12-06 19:41:31 +0200

    update

--- 2_paskaita/index.js
+++ 2_paskaita/index.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 // req - request - paduoda useris
 // res - response - grazina kai kviecia
 app.get("/", (req, res) => {
     res.send('Hello World!');
 });
+
+app.listen(port, () => {
+    console.log(`Server is running on the ${port} port`);
+});
+
