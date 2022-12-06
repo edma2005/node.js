58704d602a6a2ac4d2dafc68b1ee7f54038be601
Edma <edma20052005@yahoo.co.uk>
2022-12-06 21:45:06 +0200

    update

--- 2_paskaita/index.js
+++ 2_paskaita/index.js
@@ -5,15 +5,25 @@ const port = 3000;
 
 app.use(cors());
 
-const cars = ["BMW", "Porsche", "VW"];
+// const cars = ["BMW", "Porsche", "VW"];
+
+// // req - request - paduoda useris
+// // res - response - grazina kai kviecia
+// app.get("/", (req, res) => {
+//     // res.send('Hello World!');
+//     res.send(cars);
+// });
+
+const users = ["Alex", "Rose", "Megan"];
 
 // req - request - paduoda useris
 // res - response - grazina kai kviecia
 app.get("/", (req, res) => {
     // res.send('Hello World!');
-    res.send(cars);
+    console.log(users);
+    res.send(users);
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on the ${port} port`);
+    console.log(`Server is running on the 127.0.0.1:${port} port`);
 });
