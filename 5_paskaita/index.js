9087e2317cec6bafcf93337443ee30f47953d556
Edma <edma20052005@yahoo.co.uk>
2022-12-20 18:57:07 +0200

    changes

--- 5_paskaita/index.js
+++ 5_paskaita/index.js
@@ -12,27 +12,61 @@ const client = new MongoClient(uri);
 app.use(cors());
 app.use(express.json());
 
-// app.get('/', async (req, res) => {
+app.get('/', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con
+    .db('first')
+    .collection('orders')
+    .insertMany([
+      {product: 'toothbrush', total: 5.75, customer: 'John'},
+      {product: 'guitar', total: 55.5, customer: 'Bob'},
+      {product: 'milk', total: 2.85, customer: 'Susan'},
+      {product: 'pizza', total: 18, customer: 'Peter'},
+      {product: 'boots', total: 65.99, customer: 'Tomas'},
+      {product: 'cats', total: 99.99, customer: 'Mike'}
+    ]);
+    // .count({product: 'tootbrush'})
+    // .distinct('product')
+      await con.close();
+      res.send(data);
+    } catch (error) {
+        res.status(500).send({ error });
+    }
+    });
+
+// app.get("/categories", async (req, res) => {
 //   try {
 //     const con = await client.connect();
 //     const data = await con
-//     .db('first')
-//     .collection('orders')
-//     .insertMany([
-//       {product: 'toothbrush', total: 4.75, customer: 'Mike'},
-//       {product: 'guitar', total: 4.75, customer: 'Mike'},
-//       {product: 'milk', total: 4.75, customer: 'Mike'},
-//       {product: 'pizza', total: 4.75, customer: 'Mike'},
-//       {product: 'boots', total: 4.75, customer: 'Mike'},
-//       {product: 'cats', total: 4.75, customer: 'Mike'}
-//     ]);
-//       await con.close();
-//       res.send(data);
-//     } catch (error) {
-//         res.status(500).send({ error });
-//     }
-//     });
-
+//       .db("shop")
+//       .collection("categories")
+//       .aggregate([
+//         {
+//           $lookup: {
+//             from: "products",
+//             localField: "title",
+//             foreignField: "category",
+//             as: "products",
+//           },
+//         },
+//         {
+//           $project: {
+//             category: "$title",
+//             total: {
+//               $sum: "$products.price",
+//             },
+//           },
+//         },
+//       ])
+//       .toArray();
+//     console.log(data);
+//     res.send(data);
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
+    
 app.listen(port, () => {
     console.log(`It works on 127.0.0.1:${port} port`);
   });
