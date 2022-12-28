88dd7389f92f1314d1a8b3bc575b6a669227fba4
Edma <edma20052005@yahoo.co.uk>
2022-12-28 20:09:38 +0200

    auth update

--- jwtProject/app.js
+++ jwtProject/app.js
@@ -13,34 +13,27 @@ app.use(express.json({ limit: "50mb" }));
 
 app.post("/register", async (req, res) => {
   try {
-    // Get user input
     const { first_name, last_name, email, password } = req.body;
 
-    // Validate user input
     if (!(email && password && first_name && last_name)) {
       res.status(400).send("All input is required");
     }
 
-    // check if user already exist
-    // Validate if user exist in our database
     const oldUser = await User.findOne({ email });
 
     if (oldUser) {
       return res.status(409).send("User Already Exist. Please Login");
     }
 
-    //Encrypt user password
     encryptedPassword = await bcrypt.hash(password, 10);
 
-    // Create user in our database
     const user = await User.create({
       first_name,
       last_name,
-      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      email: email.toLowerCase(),
       password: encryptedPassword,
     });
 
-    // Create token
     const token = jwt.sign(
       { user_id: user._id, email },
       process.env.TOKEN_KEY,
@@ -48,10 +41,8 @@ app.post("/register", async (req, res) => {
         expiresIn: "2h",
       }
     );
-    // save user token
     user.token = token;
 
-    // return new user
     res.status(201).json(user);
   } catch (err) {
     console.log(err);
@@ -60,18 +51,14 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   try {
-    // Get user input
     const { email, password } = req.body;
 
-    // Validate user input
     if (!(email && password)) {
       res.status(400).send("All input is required");
     }
-    // Validate if user exist in our database
     const user = await User.findOne({ email });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      // Create token
       const token = jwt.sign(
         { user_id: user._id, email },
         process.env.TOKEN_KEY,
@@ -80,10 +67,8 @@ app.post("/login", async (req, res) => {
         }
       );
 
-      // save user token
       user.token = token;
 
-      // user
       res.status(200).json(user);
     }
     res.status(400).send("Invalid Credentials");
@@ -96,7 +81,6 @@ app.get("/welcome", auth, (req, res) => {
   res.status(200).send("Welcome 🙌 ");
 });
 
-// This should be the last route else any after it won't work
 app.use("*", (req, res) => {
   res.status(404).json({
     success: "false",
